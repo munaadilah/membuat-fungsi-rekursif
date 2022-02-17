9871e22512fb72cbc0fb0847fe0e1f8e74b4a431
Muna 'Adilah <93821740+munaadilah@users.noreply.github.com>
2022-02-17 22:25:33 +0700

    Add files via upload

---
+++ fibonaci.py
@@ -0,0 +1,10 @@
+a = int(input("Masukkan banyaknya bilangan : "))
+b = 1
+c = 0
+
+for i in range(a):
+    i += 0
+    print("Bilangan fibonacci ke-", i,"adalah", c)
+    total = b + c
+    b = c
+    c = total
---
+++ rekursif.py
@@ -0,0 +1,17 @@
+def faktorial(n):
+    if n == 1:
+        return 1  
+    else:
+        return n * faktorial(1)
+
+print("Bilangan fibonacci ke-0 adalah", faktorial(0))
+print("Bilangan fibonacci ke-1 adalah", faktorial(1))
+print("Bilangan fibonacci ke-2 adalah", faktorial(1))
+print("Bilangan fibonacci ke-3 adalah", faktorial(2))
+print("Bilangan fibonacci ke-4 adalah", faktorial(3))
+print("Bilangan fibonacci ke-5 adalah", faktorial(5))
+print("Bilangan fibonacci ke-6 adalah", faktorial(8))
+print("Bilangan fibonacci ke-7 adalah", faktorial(13))
+print("Bilangan fibonacci ke-8 adalah", faktorial(21))
+print("Bilangan fibonacci ke-9 adalah", faktorial(34))
+print("Bilangan fibonacci ke-10 adalah", faktorial(55))
